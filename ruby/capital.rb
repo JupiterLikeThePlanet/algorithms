3ab2ec20e476f3f50a4838ae83809629bc7d5c31
JupiterLikeThePlanet <jupiterbaudot@gmail.com>
2016-10-11 16:12:32 -0700

    Add space removal at first position

--- ruby/capital.rb
+++ ruby/capital.rb
@@ -6,19 +6,6 @@
 # Example: "this is a test... and another test."
 # Output:  "This is a test... And another test."
 
-
-######################################
-# def capitalize(phrase)
-# 	sentence = phrase.split(".")
-# 	# p sentence
-# 	sentence.map {|s| s.capitalize!}
-# 	answer = sentence.join(". ")
-# 	# p answer
-# 	return answer
-# end
-######################################
-######################################
-######################################
 def capitalize(phrase)
 	if phrase.include?("...")
 		array = phrase.split(" ")
@@ -39,23 +26,21 @@ def capitalize(phrase)
 		 return answer	 
 	else
 		array = phrase.split(".")
-		array.map {|s| s.capitalize!}
-		# ind_num = 0
-		# if element.include?(".")
-		# 	ind_num = ind
-	 # 		unless array[ind_num+1] == nil 
-	 # 			array[ind_num+1].capitalize!
-	 # 		end 
-	 # 	else
-	 # 		next
-	 # 	end
+		array.map do |s| 
+			if s[0] == " "
+				s.slice!(0)
+				s.capitalize!
+			else
+				s.capitalize!
+			end
+		end
+
 		answer = array.join(". ")
 		p answer
 		return answer
 	end
 end
-######################################
-######################################
+
 capitalize("this is a test... and another test.")
 capitalize("first, solve the problem.then, write the code.")
 capitalize("hello, dr. morris.")
